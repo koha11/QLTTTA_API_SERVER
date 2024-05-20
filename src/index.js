571f58cb9c69b8f55aba51c676c194ee75a3163a
= <=>
2024-05-20 23:41:28 +0700

    SET CORS

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+});
+
 //app.use(cors);
 // Init route
 route(app);
