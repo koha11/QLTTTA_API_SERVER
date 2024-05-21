38470bf2742300b8dc6420eb22a7c1d13f2457a9
= <=>
2024-05-21 20:44:44 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -11,14 +11,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-});
-
 // Init route
 route(app);
 
