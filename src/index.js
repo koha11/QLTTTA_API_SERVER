d3eb177b193b2dbb37c6fcdb3a837dd478785c5c
= <=>
2024-05-21 20:38:13 +0700

    update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+});
+
 // Init route
 route(app);
 
