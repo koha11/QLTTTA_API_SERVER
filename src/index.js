814863b6ebec3e279022423e5f8c61c7f0a2a21f
= <=>
2024-05-25 17:44:29 +0700

    update

--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+);
+
 // Init route
 route(app);
 
