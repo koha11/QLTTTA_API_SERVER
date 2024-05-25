401f608cc11e600003af81ab14834a0934380754
= <=>
2024-05-25 17:00:42 +0700

    update

--- src/Controller/StudentController.js
+++ src/Controller/StudentController.js
@@ -36,15 +36,12 @@ class StudentController {
     let input = req.body;
     const updateObj = spFunc.updateDataStr(input);
     console.log(updateObj);
-    res.header('Access-Control-Allow-Origin', '*');
-    // res.header(
-    //   'Access-Control-Allow-Headers',
-    //   'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    // );
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
     excStmt(`update student set ${updateObj.set} where ${updateObj.id} `).then(
       (value) => {
-        console.log(value);
-        res.send(value);
+        res.send('');
       }
     );
   }
